e7362ed4eabc7fdab592ec434e99f23a0dc81ff7
Aaron Bushell <aaron_bushell@yahoo.com>
2022-04-09 16:00:31 +0000

    add imports and function calls from packages to main()

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/aaronbushell1984/golangUdemy/dependency"
+	"github.com/aaronbushell1984/golangUdemy/exampleimport"
+	"github.com/aaronbushell1984/golangUdemy/module"
+)
 
 func main() {
 	fmt.Println("Main package and main function point is entry point in go")
-	
+	module.HelloModule()
+	fmt.Println(dependency.HelloDependency(), "from the dependency module")
+	exampleimport.ExampleImportPrint()
 }
