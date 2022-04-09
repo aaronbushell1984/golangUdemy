2eaf84f529b84c50004147996d2ee2f7f7c0a246
Aaron Bushell <aaron_bushell@yahoo.com>
2022-04-09 16:58:27 +0000

    add example of _ blank identifier to capturereturns

--- capturereturns/capturereturns.go
+++ capturereturns/capturereturns.go
@@ -9,3 +9,11 @@ func CaptureReturnsPrint() {
 	fmt.Println(n)
 	fmt.Println(err)
 }
+
+// fmt.Println - func Println(a ...any) (n int, err error)
+// return of err can be discarded with _ (blank identifier)
+func CaptureReturnsVoidErrPrint() {
+	n, _ := fmt.Println("Capturing return values from fmt.Println:")
+	fmt.Println(n)
+	//fmt.Println(err) this no throws compile error because err is _
+}
