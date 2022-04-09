e3f6f4e4a1cd98d31ff268dde38ff5008b17d078
Aaron Bushell <aaron_bushell@yahoo.com>
2022-04-09 17:13:13 +0000

    add variables and some languuage idiom examples

---
+++ variables/variables.go
@@ -0,0 +1,17 @@
+package main 
+
+import "fmt"
+
+func main() {
+	// go language idiom
+	// short declaration operator - "declares" and "assigns" variable
+	x := 42
+	fmt.Println(x)
+	// assign new value
+	x = 99
+	fmt.Println(x)
+	// statements are usually oneline is one line and made of expressions (100 + 24 or 24 etc)
+	y := 100 + 24
+	fmt.Println(y)
+
+}
