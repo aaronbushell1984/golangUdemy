42f742a69551d17e0242e9088fa619d258897527
Aaron Bushell <aaron_bushell@yahoo.com>
2022-04-09 16:46:15 +0000

    add .go to end of variadicparameter - add func to function declaration

--- variadicparameter/variadicparameter.go
+++ variadicparameter/variadicparameter.go
@@ -5,6 +5,6 @@ import "fmt"
 // from fmt docs 
 // func Println(a ...any) (n int, err error) - takes any number of any types of arguments (variadic parameter)
 
-VaridaicParameterPrint() {
-	fmt.Println("fmt.Prinln can accept any number of different type variable such as this string and following number and boolean", 42, true)
+func VaridaicParameterPrint() {
+	fmt.Println("fmt.Prinln can accept any number of different type variable such as this string and following number and boolean:", 42, true)
 }
