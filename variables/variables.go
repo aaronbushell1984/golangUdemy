598b58437f5bd2808008ed6df53d8144745b9403
Aaron Bushell <aaron_bushell@yahoo.com>
2022-04-24 15:31:08 +0000

    add tests for local and global scope

--- variables/variables.go
+++ variables/variables.go
@@ -2,23 +2,31 @@ package variables
 
 import "fmt"
 
-func PrintReassignedVariables() int {
+func GetReassignedVariables() int {
 	// go language idiom
 	// short declaration operator - "declares" and "assigns" variable
 	x := 42
 	fmt.Println(x)
 	// assign new value
 	x = 99
-	fmt.Println(x)
 	return x
 }
 
-func PrintExpressionVariables() int {
-		// statements are usually oneline is one line and made of expressions (100 + 24 or 24 etc)
+func GetExpressionVariables() int {
+	// statements are usually oneline is one line and made of expressions (100 + 24 or 24 etc)
 	// operator is + operands 100 and 24 in this example
 	y := 100 + 24
-	fmt.Println(y)
 	return y
 }
 
+func GetLocalVariables() string {
+	// short declaration operator has local scope
+	local := "Available in function"
+	return local
+}
 
+var global = "Available outside function"
+
+func GetGlobalVariables() string {
+	return global
+}
--- variables/variables_test.go
+++ variables/variables_test.go
@@ -4,14 +4,28 @@ import "testing"
 
 func TestPrintReassignedVariables(t *testing.T) {
 	want := 99
-	if got := PrintReassignedVariables(); got != want {
-		t.Errorf("PrintReassignedVariables() = %q, want %q", got, want)
+	if got := GetReassignedVariables(); got != want {
+		t.Errorf("GetReassignedVariables() = %q, want %q", got, want)
 	}
 }
 
 func TestPrintExpressionVariables(t *testing.T) {
 	want := 124
-	if got := PrintExpressionVariables(); got != want {
-		t.Errorf("PrintExpressionVariables() = %q, want %q", got, want)
+	if got := GetExpressionVariables(); got != want {
+		t.Errorf("GetExpressionVariables() = %q, want %q", got, want)
+	}
+}
+
+func TestGetLocalVariables(t *testing.T) {
+	want := "Available in function"
+	if got := GetLocalVariables(); got != want {
+		t.Errorf("GetLocalVariables() = %q, want %q", got, want)
+	}
+}
+
+func TestGetGlobalVariables(t *testing.T) {
+	want := "Available outside function"
+	if got := GetGlobalVariables(); got != want {
+		t.Errorf("GetGlobalVariables() = %q, want %q", got, want)
 	}
 }
