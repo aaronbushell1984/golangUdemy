c3ccc2828b204f16e278c153bab4fb085dd63e96
Aaron Bushell <aaron_bushell@yahoo.com>
2022-04-24 18:41:00 +0000

    add binary and hexidecimal using Sprintf

--- examplefmt/examplefmt.go
+++ examplefmt/examplefmt.go
@@ -4,7 +4,18 @@ import (
 	"fmt"
 )
 
+func PrintToConsole(variable interface{}) {
+	fmt.Print("Printing without formatting:", variable)
+}
+
 func GetBinaryFromInt(number int) string {
 	return fmt.Sprintf("%b", number)
 }
 
+func GetHexidecimalFromInt(number int) string {
+	return fmt.Sprintf("%x", number)
+}
+
+func GetOctalHexidecimalFromInt(number int) string {
+	return fmt.Sprintf("%#x", number)
+}
--- examplefmt/examplefmt_test.go
+++ examplefmt/examplefmt_test.go
@@ -9,3 +9,21 @@ func TestGetBinaryFromInt(t *testing.T) {
 		t.Errorf("GetGetBinaryFromInt(number) = %q, want %q", got, want)
 	}
 }
+
+func TestGetHexidecimalFromInt(t *testing.T) {
+	number := 42
+	want := "2a"
+	if got := GetHexidecimalFromInt(number); got != want {
+		t.Errorf("GetHexidecimalFromInt(number) = %q, want %q", got, want)
+	}
+}
+
+func TestGetOctalHexidecimalFromInt(t *testing.T) {
+	number := 42
+	want := "0x2a"
+	if got := GetOctalHexidecimalFromInt(number); got != want {
+		t.Errorf("GetOctalHexidecimalFromInt(number) = %q, want %q", got, want)
+	}
+}
+
+
