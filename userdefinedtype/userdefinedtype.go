a4d2f6389ef4da8c552e324811ec547fc7c566ec
Aaron Bushell <aaron_bushell@yahoo.com>
2022-04-24 20:19:57 +0000

    start user defined types

---
+++ userdefinedtype/userdefinedtype.go
@@ -0,0 +1,11 @@
+package userdefinedtype
+
+// import "fmt"
+
+// type mytype int
+
+// func PrintType(mytype int) string {
+// 	nametext := "My type is called: "
+// 	typetext := " and is of underlying type: "
+// 	return fmt.Sprintf("%s%s%s%s", nametext, mytype, typetext, mytype)
+// }
---
+++ userdefinedtype/userdefinedtype_test.go
@@ -0,0 +1,10 @@
+package userdefinedtype
+
+import "testing"
+
+func TestPrintTypeInt(t *testing.T) {
+// 	want := "My type is: " + mytype + " and is of underlying type: " + underlyingtype
+// 	if got := PrintType(typename); got != want {
+// 		t.Errorf("CreateType(mytype, int) = %q, want %q", got, want)
+// 	}
+}
