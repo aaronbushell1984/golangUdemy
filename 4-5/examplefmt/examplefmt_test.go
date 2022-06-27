87edd5838d0eb9025e3488baae18328f0e69c014
Aaron Bushell <aaron_bushell@yahoo.com>
2022-06-27 21:39:04 +0000

    Add example fmt documentation/examples

--- 4-5/examplefmt/examplefmt_test.go
+++ 4-5/examplefmt/examplefmt_test.go
@@ -2,6 +2,7 @@ package examplefmt
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 )
 
@@ -13,12 +14,24 @@ func TestGetBinaryFromInt(t *testing.T) {
 	}
 }
 
+func ExampleGetBinaryFromInt() {
+	fmt.Println(GetBinaryFromInt(42))
+	// Output:
+	// 101010
+}
+
 func TestGetHexidecimalFromInt(t *testing.T) {
 	number := 42
 	want := "2a"
 	if got := GetHexidecimalFromInt(number); got != want {
 		t.Errorf("GetHexidecimalFromInt(number) = %q, want %q", got, want)
 	}
+}
+
+func ExampleGetHexidecimalFromInt() {
+	fmt.Println(GetHexidecimalFromInt(42))
+	// Output:
+	// 2a
 }
 
 func TestGetOctalHexidecimalFromInt(t *testing.T) {
@@ -29,23 +42,46 @@ func TestGetOctalHexidecimalFromInt(t *testing.T) {
 	}
 }
 
+func ExampleGetOctalHexidecimalFromInt() {
+	fmt.Println(GetOctalHexidecimalFromInt(42))
+	// Output:
+	// 0x2a
+}
+
 func TestPrintConsoleAndSaveToBuffer(t *testing.T) {
 	word := "test"
 	want := "Printing without formatting: test"
 	var output bytes.Buffer
-	PrintConsoleAndSaveToBuffer(&output, word);
+	PrintConsoleAndSaveToBuffer(&output, word)
 	if got := output.String(); got != want {
 		t.Errorf("PrintConsoleAndSaveToBuffer(os.Stdout, test) = %q, want %q", got, want)
 	}
 }
 
+func ExamplePrintConsoleAndSaveToBuffer() {
+	var output bytes.Buffer
+	PrintConsoleAndSaveToBuffer(&output, "test")
+	fmt.Println(output.String())
+	// Output:
+	// Printing without formatting: test
+}
+
 func TestPrintfConsoleAndSaveToBuffer(t *testing.T) {
 	word := "test"
-	want := `Printing with formatting: 
+	want := `Printing with formatting:
 	test`
 	var output1 bytes.Buffer
-	PrintfConsoleAndSaveToBuffer(&output1, word);
+	PrintfConsoleAndSaveToBuffer(&output1, word)
 	if got := output1.String(); got != want {
 		t.Errorf("PrintfConsoleAndSaveToBuffer(os.Stdout, test) = %q, want %q", got, want)
 	}
+}
+
+func ExamplePrintfConsoleAndSaveToBuffer() {
+	var output bytes.Buffer
+	PrintfConsoleAndSaveToBuffer(&output, "test")
+	fmt.Println(output.String())
+	// Output:
+	// Printing with formatting:
+	//	test
 }
