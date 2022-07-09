b4da49aa86c23168a7dd64f0c348448b5db4e92c
Aaron Bushell <aaron_bushell@yahoo.com>
2022-07-09 22:20:43 +0000

    complete excercisetwo

--- 7/excercisetwo/excercisetwo.go
+++ 7/excercisetwo/excercisetwo.go
@@ -68,12 +68,60 @@ func CheckGreaterThan(i int, i2 int) bool {
 	}
 }
 
-// ReturnConstTrafficLights returns traffic light constants
+// ReturnConstTrafficLights returns traffic light constants.
+//
+// The first light is a constant of a kind and the second two are not:
+//	const (
+//		light1 string = "RED"
+//		light2 = "AMBER"
+//		light3 = "GREEN"
+//	)
 func ReturnConstTrafficLights() string {
 	const (
-		light1 = "RED"
-		light2 = "AMBER"
-		light3 = "GREEN"
+		light1 string = "RED"
+		light2        = "AMBER"
+		light3        = "GREEN"
 	)
 	return fmt.Sprintf("%v, %v, %v", light1, light2, light3)
+}
+
+// PrintIntBeforeAndAfterBitShift takes an int and returns the decimal, binary and hex before and after bit shift
+func PrintIntBeforeAndAfterBitShift(i int) string {
+	before := fmt.Sprintf("Before: %v %b %#x", i, i, i)
+	i = i << 1
+	after := fmt.Sprintf("After: %v %b %#x", i, i, i)
+	return before + ", " + after
+}
+
+// MaintainStringFormatting returns the same passed in string.
+//
+// The test demonstrates it maintains raw string literal:
+//	func TestMaintainStringFormatting(t *testing.T) {
+//		want := "This maintains formatting using a\nRaw\nString\nLiteral"
+//		example := `This maintains formatting using a
+//	Raw
+//	String
+//	Literal`
+//		if got := MaintainStringFormatting(example); got != want {
+//			t.Errorf("MaintainStringFormatting(): got: %q want: %q", got, want)
+//		}
+//	}
+func MaintainStringFormatting(s string) string {
+	return fmt.Sprintf("%v", s)
+}
+
+// ReturnLastFourYears takes the year and returns the previous four using iota
+func ReturnLastFourYears(year int) string {
+	const (
+		_ = iota
+		y1
+		y2
+		y3
+		y4
+	)
+	year1 := year - y1
+	year2 := year - y2
+	year3 := year - y3
+	year4 := year - y4
+	return fmt.Sprintf("%v, %v, %v, %v", year1, year2, year3, year4)
 }
--- 7/excercisetwo/excercisetwo_test.go
+++ 7/excercisetwo/excercisetwo_test.go
@@ -115,3 +115,34 @@ func TestReturnConstTrafficLights(t *testing.T) {
 		t.Errorf("ReturnConstTrafficLights: Got: %q, Want: %q", got, want)
 	}
 }
+
+func ExamplePrintIntBeforeAndAfterBitShift() {
+	fmt.Println(PrintIntBeforeAndAfterBitShift(1))
+	fmt.Println(PrintIntBeforeAndAfterBitShift(10))
+	fmt.Println(PrintIntBeforeAndAfterBitShift(32))
+	// Output:
+	// Before: 1 1 0x1, After: 2 10 0x2
+	// Before: 10 1010 0xa, After: 20 10100 0x14
+	// Before: 32 100000 0x20, After: 64 1000000 0x40
+}
+
+func TestMaintainStringFormatting(t *testing.T) {
+	want := "This maintains formatting using a\nRaw\nString\nLiteral"
+	example := `This maintains formatting using a
+Raw
+String
+Literal`
+	if got := MaintainStringFormatting(example); got != want {
+		t.Errorf("MaintainStringFormatting(): got: %q want: %q", got, want)
+	}
+}
+
+func ExampleReturnLastFourYears() {
+	fmt.Println(ReturnLastFourYears(1984))
+	fmt.Println(ReturnLastFourYears(2022))
+	fmt.Println(ReturnLastFourYears(999))
+	// Output:
+	// 1983, 1982, 1981, 1980
+	// 2021, 2020, 2019, 2018
+	// 998, 997, 996, 995
+}
