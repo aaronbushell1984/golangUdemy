7dc28abd05ea2c1822073e2d9c8d205c5426ade1
Aaron Bushell <aaron_bushell@yahoo.com>
2022-07-10 11:58:04 +0000

    add loops benchmark tests

--- 8/loops/loops_test.go
+++ 8/loops/loops_test.go
@@ -1,6 +1,9 @@
 package loops
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func ExamplePrintNumbersLoop() {
 	fmt.Println(PrintNumbersLoop(0, 1))
@@ -12,8 +15,20 @@ func ExamplePrintNumbersLoop() {
 	// [-10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10]
 }
 
+func BenchmarkPrintNumbersLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PrintNumbersLoop(-10, 10)
+	}
+}
+
 func ExampleReturnAllTwoBitOptions() {
 	fmt.Println(ReturnAllTwoBitOptions())
 	// Output:
 	// [00 01 10 11]
+}
+
+func BenchmarkReturnAllTwoBitOptions(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ReturnAllTwoBitOptions()
+	}
 }
