c888d6444c582baf0b0469351b34399ecc535709
Aaron Bushell <aaron_bushell@yahoo.com>
2022-07-10 00:49:55 +0000

    add nested loop example

--- 8/loops/loops.go
+++ 8/loops/loops.go
@@ -1,12 +1,28 @@
 // Package loops demonstrates the use of loops in go
 package loops
 
+import (
+	"fmt"
+)
+
 // PrintNumbersLoop returns all numbers between start and end inputs using syntax:
 //	for initialize; condition; post
 func PrintNumbersLoop(start int, end int) []int {
 	var numbers []int
 	for i := start; i <= end; i++ {
 		numbers = append(numbers, i)
+	}
+	return numbers
+}
+
+// ReturnAllTwoBitOptions returns all binary options when using two bits
+func ReturnAllTwoBitOptions() []string {
+	var numbers []string
+	for i := 0; i <= 1; i++ {
+		for k := 0; k <= 1; k++ {
+			binary := fmt.Sprintf("%d%d", i, k)
+			numbers = append(numbers, binary)
+		}
 	}
 	return numbers
 }
--- 8/loops/loops_test.go
+++ 8/loops/loops_test.go
@@ -11,3 +11,9 @@ func ExamplePrintNumbersLoop() {
 	// [0 1 2 3 4 5 6 7 8 9 10]
 	// [-10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10]
 }
+
+func ExampleReturnAllTwoBitOptions() {
+	fmt.Println(ReturnAllTwoBitOptions())
+	// Output:
+	// [00 01 10 11]
+}
