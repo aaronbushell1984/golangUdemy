c3559ca2a41a4200e0ddf165ebe3600e304125ac
Aaron Bushell <aaron_bushell@yahoo.com>
2022-07-15 20:41:28 +0000

    add slice examples

--- 10/slices/slices.go
+++ 10/slices/slices.go
@@ -1,3 +1,4 @@
+// Package slices demonstrates the use of slices in go
 package slices
 
 import (
@@ -13,4 +14,49 @@ func ReturnSlice(compLiteral interface{}) string {
 		return "This is not a slice"
 	}
 	return fmt.Sprintf("%v", compLiteral)
+}
+
+// GetFromSliceByIndex returns the integer from the provided slice at the index provided
+func GetFromSliceByIndex(sliceOfInt []int, index int) int {
+	return sliceOfInt[index]
+}
+
+// GetAllFromSlice returns all integers from slice as a string
+//
+// Demonstrates use of a "for range" which discards index:
+//	for _, v := range sliceOfInt {
+//		result = append(result, v)
+//	}
+func GetAllFromSlice(sliceOfInt []int) string {
+	var result []int
+	for _, v := range sliceOfInt {
+		result = append(result, v)
+	}
+	return fmt.Sprint(result)
+}
+
+// GetSliceOfSlice returns a portion of a slice
+//
+// N.B. Indexes start at 0 -- AND
+//	[start:startOfEnd]
+// may not be intuitive
+func GetSliceOfSlice(slice []int, start int, startOfEnd int) []int {
+	return slice[start:startOfEnd]
+}
+
+// GetAppendedSlices returns the second slice appended to the end of the first
+//
+// Uses:
+//	...
+// Which unpacks slice2:
+//	append(slice1, slice2...)
+// Not be confused with:
+//	...T
+// Which in a function signature is a variadic parameter meaning, it can accept any number of arguments of the given type.
+func GetAppendedSlices(slice1 []int, slice2 []int) []int {
+	return append(slice1, slice2...)
+}
+
+func GetDeletedSlice(slice []int, start1 int, end1 int, start2 int, end2 int) []int {
+	return append(slice[start1:end1+1], slice[start2:end2+1]...)
 }
--- 10/slices/slices_test.go
+++ 10/slices/slices_test.go
@@ -14,3 +14,49 @@ func ExampleReturnSlice() {
 	// [1 2 3]
 	// This is not a slice
 }
+
+func ExampleGetFromSliceByIndex() {
+	integers := []int{1, 2, 3}
+	fmt.Println(GetFromSliceByIndex(integers, 2))
+	fmt.Println(GetFromSliceByIndex(integers, 0))
+	// Output:
+	// 3
+	// 1
+}
+
+func ExampleGetAllFromSlice() {
+	integers := []int{1, 2, 3}
+	moreIntegers := []int{-11, 2, 333, 9}
+	fmt.Println(GetAllFromSlice(integers))
+	fmt.Println(GetAllFromSlice(moreIntegers))
+	// Output:
+	// [1 2 3]
+	// [-11 2 333 9]
+}
+
+func ExampleGetSliceOfSlice() {
+	integersEasy := []int{0, 1, 2, 3, 4, 5}
+	integersCaution := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(GetSliceOfSlice(integersEasy, 0, 4))
+	fmt.Println(GetSliceOfSlice(integersCaution, 0, 4))
+	// Output:
+	// [0 1 2 3]
+	// [1 2 3 4]
+}
+
+func ExampleGetAppendedSlices() {
+	slice1 := []int{0, 1, 2, 3, 4, 5}
+	slice2 := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(GetAppendedSlices(slice1, slice2))
+	fmt.Println(GetAppendedSlices(slice2, slice1))
+	// Output:
+	// [0 1 2 3 4 5 1 2 3 4 5 6]
+	// [1 2 3 4 5 6 0 1 2 3 4 5]
+}
+
+func ExampleGetDeletedSlice() {
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(GetDeletedSlice(slice, 0, 2, 5, 6))
+	// Output:
+	// [0 1 2 5 6]
+}
