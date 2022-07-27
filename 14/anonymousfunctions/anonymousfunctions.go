fa012a380f748353dcd34583690a5c34b530645d
Aaron <aaron_bushell@yahoo.com>
2022-07-27 15:11:52 +0100

    move anonymous function files inside folder

--- 14/anonymousfunctions/anonymousfunctions.go
+++ 14/anonymousfunctions/anonymousfunctions.go
@@ -2,9 +2,11 @@
 package anonymousfunctions
 
 // AnonymousFunction contains an un-named self executing function:
+//
 //	func() {
 //		result = "I was called by an anonymous function inside a named one"
 //	}()
+//
 // Paradoxically it is contained in a named function to allow the example test
 func AnonymousFunction() string {
 	result := ""
