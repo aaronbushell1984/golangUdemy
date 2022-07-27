1788a4471773c793ca8163ae9b08bed45b9e7016
Aaron <aaron_bushell@yahoo.com>
2022-07-27 01:30:43 +0100

    part complete perishable flower example

--- 14/polymorphism/polymorphism.go
+++ 14/polymorphism/polymorphism.go
@@ -1,25 +1,94 @@
 // Package polymorphism demonstrates polymorphism in go
+//
+// Declaring the interface perishable defines the behaviour to spoil food:
+//
+//	type perishable interface {
+//		Spoil() food
+//	}
+//
+// Attaching the spoil method to type food:
+//
+//	func (f food) Spoil() food {
+//		f.spoiled = true
+//		return f
+//	}
+//
+// Allows an apple to be of type food AND perishable:
+//
+//	func ExampleTestIsPerishable() {
+//		apple := MakeFood("apple", "red", true)
+//		ten := 10
+//		fmt.Println(TestIsPerishable(apple))
+//		fmt.Println(TestIsPerishable(ten))
+//		// Output:
+//		// true
+//		// false
+//		}
 package polymorphism
 
-type hop struct {
-	name string
-	ibu  int
+type food struct {
+	name    string
+	colour  string
+	spoiled bool
 }
 
-type grain struct {
-	name string
+type flower struct {
+	name    string
+	colour  string
+	spoiled bool
 }
 
-type recipe struct {
-	name   string
-	grains []grain
-	hops   []hop
-	yeast  string
+// perishable represents behaviour to spoil
+type perishable interface {
+	Spoil() food
 }
 
-func BrewBeer(rec recipe) int {
-	if rec.name == "ipa" {
-		return 20
+// MakeFood constructs a variable of type food
+func MakeFood(nme string, clr string, spld bool) food {
+	return food{
+		nme,
+		clr,
+		spld,
 	}
-	return 30
+}
+
+// MakeFlower constructs a variable of type flower
+func MakeFlower(nme string, clr string, spld bool) flower {
+	return flower{
+		nme,
+		clr,
+		spld,
+	}
+}
+
+// Spoil attaches to food allowing setting of spoiled field to true using:
+//
+//	food.Spoil(banana)
+func (f food) Spoil() food {
+	f.spoiled = true
+	return f
+}
+
+// Spoil attaches to flower allowing setting of spoiled field to true using:
+//
+//	flower.Spoil(rose)
+func (f flower) Spoil() flower {
+	f.spoiled = true
+	return f
+}
+
+// TestIsFood checks if the passed in variable is of type food using a type assertion:
+//
+//	t, ok := i.(T)
+func TestIsFood(underTest interface{}) bool {
+	_, ok := underTest.(food)
+	return ok
+}
+
+// TestIsPerishable checks if the passed in variable is of type perishable using a type assertion:
+//
+//	t, ok := i.(T)
+func TestIsPerishable(underTest interface{}) bool {
+	_, ok := underTest.(perishable)
+	return ok
 }
--- 14/polymorphism/polymorphism_test.go
+++ 14/polymorphism/polymorphism_test.go
@@ -2,30 +2,54 @@ package polymorphism
 
 import "fmt"
 
-func ExampleBrewBeer() {
-	g1 := grain{
-		name: "marris",
-	}
-	g2 := grain{
-		name: "naked oats",
-	}
-	h1 := hop{
-		name: "citra",
-		ibu:  4,
-	}
-	ipa := recipe{
-		name: "ipa",
-		grains: []grain{
-			g1,
-			g2,
-		},
-		hops: []hop{
-			h1,
-		},
-		yeast: "kviek",
-	}
-	fmt.Println(BrewBeer(ipa))
+func ExampleMakeFood() {
+	banana := MakeFood("banana", "yellow", false)
+	fmt.Println(banana)
 	// Output:
-	// 20
+	// {banana yellow false}
+}
 
+func ExampleMakeFlower() {
+	rose := MakeFlower("rose", "red", false)
+	fmt.Println(rose)
+	// Output:
+	// {rose red false}
+}
+
+func ExampleSpoil() {
+	banana := MakeFood("banana", "yellow", false)
+	banana = food.Spoil(banana)
+	rose := MakeFlower("rose", "red", false)
+	rose = flower.Spoil(rose)
+	fmt.Println(banana)
+	fmt.Println(rose)
+	// Output:
+	// {banana yellow true}
+	// {rose red true}
+}
+
+func ExampleTestIsFood() {
+	apple := MakeFood("apple", "red", true)
+	tulip := MakeFlower("tulip", "purple", false)
+	ten := 10
+	fmt.Println(TestIsFood(apple))
+	fmt.Println(TestIsFood(tulip))
+	fmt.Println(TestIsFood(ten))
+	// Output:
+	// true
+	// false
+	// false
+}
+
+func ExampleTestIsPerishable() {
+	pineapple := MakeFood("pineapple", "yellow", true)
+	daisy := MakeFlower("daisy", "white", false)
+	ten := 10
+	fmt.Println(TestIsPerishable(pineapple))
+	fmt.Println(TestIsPerishable(daisy))
+	fmt.Println(TestIsPerishable(ten))
+	// Output:
+	// true
+	// true
+	// false
 }
