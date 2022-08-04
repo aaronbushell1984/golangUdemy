6e0f621d049de9ab87935f08787d82e41b4c3a65
Aaron Bushell <aaron_bushell@yahoo.com>
2022-08-04 20:05:24 +0000

    add callback examples

---
+++ 14/callback/callback.go
@@ -0,0 +1,54 @@
+// Package callback demonstrates passing a function into the argument of a function aka callback
+package callback
+
+// GetSum returns the total of any number of provided int values
+func GetSum(numbers ...int) int {
+	total := 0
+	for _, v := range numbers {
+		total += v
+	}
+	return total
+}
+
+// GetEven returns only the even numbers from any number of provided numbers
+func GetEven(numbers ...int) []int {
+	var evens []int
+	for _, v := range numbers {
+		if v % 2 == 0 && v != 0 {
+			evens = append(evens, v)
+		}
+	}
+	return evens
+}
+
+// GetEvenSum returns to sum of even numbers from any given numbers using a callback:
+//	GetEvenSum(getSum func(numbers ...int) int, numbers ...int) int
+// Any function which takes any number of int and returns an int can be passed:
+//	numbers := []int{1, 2, 3, 4, 5}
+//	fmt.Println(GetEvenSum(GetSum, numbers...))
+// In this argument GetSum is passed without ()
+//
+// The getSum is taken from argument and not GetSum() from package:
+//	return getSum(GetEven(numbers...)...)
+// GetEven is taken from package and []int spread to pass to callback getSum()
+func GetEvenSum(getSum func(numbers ...int) int, numbers ...int) int {
+	return getSum(GetEven(numbers...)...)
+}
+
+// GetOdd returns only the odd numbers from any number of provided numbers
+func GetOdd(numbers ...int) []int {
+	var odds []int
+	for _, v := range numbers {
+		if v % 2 != 0 && v != 0 {
+			odds = append(odds, v)
+		}
+	}
+	return odds
+}
+
+// GetOddSum returns the sum of the odd numbers provided
+//
+// It uses GetSum() and GetOdd() in  the pacakge package to demonstrate not using a callback
+func GetOddSum(numbers ...int) int {
+	return GetSum(GetOdd(numbers...)...)
+}
---
+++ 14/callback/callback_test.go
@@ -0,0 +1,69 @@
+package callback
+
+import "fmt"
+
+func ExampleGetSum() {
+	fmt.Println(GetSum(1, 2, 3))
+	fmt.Println(GetSum(-1, 2, 3))
+	// Output:
+	// 6
+	// 4
+}
+
+func ExampleGetEven(){
+	numbers := []int{1, 2, 3, 4, 5}
+	numbers0 := []int{0, 2, 3, 4, 5}
+	numbersMinus := []int{-1, -2, -3, -4, -5}
+	fmt.Println(GetEven(numbers...))
+	fmt.Println(GetEven(numbers0...))
+	fmt.Println(GetEven(numbersMinus...))
+	// Output:
+	// [2 4]
+	// [2 4]
+	// [-2 -4]
+}
+
+func ExampleGetEvenSum(){
+	numbers := []int{1, 2, 3, 4, 5}
+	numbers0 := []int{0, 2, 3, 4, 5}
+	numbersMinus := []int{-1, -2, -3, -4, -5}
+	fmt.Println(GetEvenSum(GetSum, numbers...))
+	fmt.Println(GetEvenSum(GetSum, numbers0...))
+	fmt.Println(GetEvenSum(GetSum, numbersMinus...))
+	// Output:
+	// 6
+	// 6
+	// -6
+}
+
+func ExampleGetOdd(){
+	numbers := []int{1, 2, 3, 4, 5}
+	numbers0 := []int{0, 2, 3, 4, 5}
+	numbersMinus := []int{-1, -2, -3, -4, -5}
+	fmt.Println(GetOdd(numbers...))
+	fmt.Println(GetOdd(numbers0...))
+	fmt.Println(GetOdd(numbersMinus...))
+	// Output:
+	// [1 3 5]
+	// [3 5]
+	// [-1 -3 -5]
+}
+
+func ExampleGetOddSum(){
+	numbers := []int{1, 2, 3, 4, 5}
+	numbers0 := []int{0, 2, 3, 4, 5}
+	numbersMinus := []int{-1, -2, -3, -4, -5}
+	fmt.Println(GetOddSum(numbers...))
+	fmt.Println(GetOddSum(numbers0...))
+	fmt.Println(GetOddSum(numbersMinus...))
+	// Output:
+	// 9
+	// 8
+	// -9
+}
+
+
+
+
+
+
