d4b68006a4ff58b9d17689a9833e637c5be9cfc2
Aaron Bushell <aaron_bushell@yahoo.com>
2022-08-04 18:54:53 +0000

    add docs to returnfunc

--- 14/returnfunc/returnfunc.go
+++ 14/returnfunc/returnfunc.go
@@ -2,10 +2,14 @@
 package returnfunc
 
 // GetIntViaFunc returns a function which returns an int.
-//
+//	func GetIntViaFunc() func() int {
+//		return func() int {
+//			return 42
+//		}
+//	}
 // Capturing the function expression:
 //	theFunc := GetIntViaFunc()
-// Allows the result to be captured from the functio call:
+// Allows the result to be captured from the function call:
 //	answer := theFunc()
 func GetIntViaFunc() func() int {
 	return func() int {
