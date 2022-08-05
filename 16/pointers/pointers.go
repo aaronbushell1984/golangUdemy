9dc5b798dfafcd3b058f9ff4c003a0b42e3e53bc
Aaron Bushell <aaron_bushell@yahoo.com>
2022-08-05 22:38:36 +0000

    part complete pointers

--- 16/pointers/pointers.go
+++ 16/pointers/pointers.go
@@ -1,2 +1,18 @@
 // Package pointers demonstrates pointers in go
 package pointers
+
+// GetAddressOfValue takes any value of any type and returns the memory address
+//
+// The address is accessed with:
+//	&value
+// And returned as the type pointer of any type with:
+//	*interface{}
+func GetAddressOfValue(value interface{}) *interface{} {
+	return &value
+}
+
+// AssignToAddress takes a string and assigns a new pointer to the same address
+func AssignToAddress(variable string) string {
+	var result *string = &variable
+	return result
+}
--- 16/pointers/pointers_test.go
+++ 16/pointers/pointers_test.go
@@ -1,1 +1,19 @@
 package pointers
+
+import "fmt"
+
+func ExampleGetAddressOfValue() {
+	value := "Value"
+	fmt.Printf("%T", GetAddressOfValue(value))
+	// Output:
+	// *interface {}
+}
+
+func ExampleAssignToAddress() {
+	ban := "banana"
+	fmt.Printf("%T\n", ban)
+	fmt.Printf("%T", AssignToAddress(ban))
+	// Output:
+	// string
+	// string
+}
