ba55b803209ff78509473cc55f399c7e9e9cade9
Aaron Bushell <aaron_bushell@yahoo.com>
2022-08-07 00:03:44 +0000

    finish sort examples

--- 18/sort/sort.go
+++ 18/sort/sort.go
@@ -1,2 +1,40 @@
 // Package sort demonstrates sorting in go
 package sort
+
+import "sort"
+
+// SortIntsAscending sorts any number of numbers ascending into a slice using:
+//	sort.Ints(numbers)
+// This is the default behaviour
+func SortIntsAscending(numbers ...int) []int {
+	sort.Ints(numbers)
+	return numbers
+}
+
+// TestSortedInts returns true if any number of ints are in ascending order using:
+//	sort.IntsAreSorted(data)
+func TestSortedInts(data ...int) bool {
+	return sort.IntsAreSorted(data)
+}
+
+// StableSortPeopleAscendingByAge sorts people by name then age and guarantees stable sort
+func StableSortPeopleAscendingByAgeName(people []struct{Name string; Age int}) []struct{Name string; Age int} {
+	sort.SliceStable(people, func(i, j int) bool{
+		return people[i].Name < people[j].Name
+	})
+	sort.SliceStable(people, func(i, j int) bool{
+		return people[i].Age < people[j].Age
+	})
+	return people
+}
+
+// SortPeopleAscendingByAge sorts people by name then age and does not guarantee stable sort
+func SortPeopleAscendingByAgeName(people []struct{Name string; Age int}) []struct{Name string; Age int} {
+	sort.Slice(people, func(i, j int) bool{
+		return people[i].Name < people[j].Name
+	})
+	sort.Slice(people, func(i, j int) bool{
+		return people[i].Age < people[j].Age
+	})
+	return people
+}
--- 18/sort/sort_test.go
+++ 18/sort/sort_test.go
@@ -1,1 +1,59 @@
 package sort
+
+import "fmt"
+
+func ExampleSortIntsAscending() {
+	numbers := []int{3, 2, 4, 1}
+	fmt.Print(SortIntsAscending(numbers...))
+	// Output:
+	// [1 2 3 4]
+}
+
+func ExampleTestSortedInts() {
+	unsorted := []int{3, 2, 4, 1}
+	sorted := []int{1, 2, 3, 4}
+	fmt.Println(TestSortedInts(unsorted...))
+	fmt.Println(TestSortedInts(sorted...))
+	// Output:
+	// false
+	// true
+}
+
+func ExampleStableSortPeopleAscendingByAgeName() {
+	people := []struct {
+		Name string
+		Age  int
+	}{
+		{"Alice", 25},
+		{"Elizabeth", 75},
+		{"Alice", 75},
+		{"Bob", 75},
+		{"Alice", 75},
+		{"Bob", 25},
+		{"Colin", 25},
+		{"Elizabeth", 25},
+	}
+	fmt.Println(StableSortPeopleAscendingByAgeName(people))
+	// Output:
+	// [{Alice 25} {Bob 25} {Colin 25} {Elizabeth 25} {Alice 75} {Alice 75} {Bob 75} {Elizabeth 75}]
+}
+
+func ExampleSortPeopleAscendingByAgeName() {
+	people := []struct {
+		Name string
+		Age  int
+	}{
+		{"Alice", 25},
+		{"Elizabeth", 75},
+		{"Alice", 75},
+		{"Bob", 75},
+		{"Alice", 75},
+		{"Bob", 25},
+		{"Colin", 25},
+		{"Elizabeth", 25},
+	}
+	fmt.Println(SortPeopleAscendingByAgeName(people))
+	// Output:
+	// [{Alice 25} {Elizabeth 25} {Bob 25} {Colin 25} {Alice 75} {Bob 75} {Elizabeth 75} {Alice 75}]
+}
+
