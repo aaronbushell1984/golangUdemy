ecfab787d0294a6c57ee3ed421bfe694eb7dd089
Aaron Bushell <aaron_bushell@yahoo.com>
2022-08-07 23:29:24 +0000

    add examples to exercisenine

--- 21/exercisenine/exercisenine.go
+++ 21/exercisenine/exercisenine.go
@@ -1,9 +1,37 @@
 // Package exercisenine is the ninth set of exercises from the course
+//
+// A pointer receiver is set on the Person struct:
+//	func (p *Person) Speak() string {
+//		return p.name
+//	}
+// A human interface defines that any type which implements "Speak() string" is ALSO of type human:
+//	type human interface {
+//		Speak() string
+//	}
+// It is therefore possible to accept a person address:
+//	SpeakName(&jb)
+// into a function with a human argument:
+//	func SpeakName(h human) string {
+//		return h.Speak()
+//	}
+// It is NOT possible to pass in a person type as human has a pointer receiver:
+//	SpeakName(jb) -- does not compile
 package exercisenine
 
-import "sync"
+import (
+	"runtime"
+	"sync"
+)
 
 var wg sync.WaitGroup
+
+type Person struct{
+	name string
+}
+
+type human interface {
+	Speak() string
+}
 
 // TwoGoRoutines launches two go routines which append to a slice of strings:
 //	go func() {
@@ -34,4 +62,32 @@ func TwoGoRoutines() []string {
 	}()
 	wg.Wait()
 	return strings
+}
+
+// Speak attaches to Person as a pointer reciever and returns their name
+func (p *Person) Speak() string {
+	return p.name
+}
+
+// SpeakName accepts a human which allows pointer to a person but not person
+func SpeakName(h human) string {
+	return h.Speak()
+}
+
+// RaceConditionIncrementor creates a race condition as go routines are accessing the same memory space
+func RaceConditionIncrementor() int {
+	wg.Add(100)
+	var counter int
+	for i := 0; i < 100; i++ {
+		go func() {
+			res := counter
+			runtime.Gosched()
+			res++
+			counter = res
+			wg.Done()
+		}()
+		
+	}
+	wg.Wait()
+	return counter
 }
--- 21/exercisenine/exercisenine_test.go
+++ 21/exercisenine/exercisenine_test.go
@@ -8,3 +8,26 @@ func ExampleTwoGoRoutines() {
 	// [I'm a go routine I'm a go routine]
 }
 
+func ExamplePerson_Speak() {
+	jb := Person{
+		name: "James",
+	}
+	fmt.Println(jb.Speak())
+	// Output:
+	// James
+}
+
+func ExampleSpeakName() {
+	jb := Person{
+		name: "James",
+	}
+	fmt.Println(SpeakName(&jb))
+	// Output:
+	// James
+}
+
+func ExampleRaceConditionIncrementor() {
+	fmt.Println(RaceConditionIncrementor() == 100)
+	// Output:
+	// false
+}
