6e76fdd5974818c55aed3273555b247cf9cf30a0
Aaron Bushell <aaron_bushell@yahoo.com>
2022-08-07 11:32:38 +0000

    complete exerciseeight

--- 19/exerciseeight/exerciseeigtht.go
+++ 19/exerciseeight/exerciseeigtht.go
@@ -4,6 +4,8 @@ package exerciseeight
 import (
 	"encoding/json"
 	"log"
+	"os"
+	"sort"
 )
 
 type user struct {
@@ -16,6 +18,13 @@ type person struct {
 	Last    string   `json:"Last"`
 	Age     int      `json:"Age"`
 	Sayings []string `json:"Sayings"`
+}
+
+type agent struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
 }
 
 
@@ -36,4 +45,50 @@ func UnmarshallPeople(data []byte, p []person) []person {
 		log.Fatal(err)
 	}
 	return p
+}
+
+// EncodeJsonStdout encodes json directly to Stdout
+//
+// In encoding/json - NewEncoder accept the writer interface:
+//	func NewEncoder(w io.Writer) *Encoder
+// In this function - os.Stdout is also of Type Writer and therefore replaces (w io.Writer):
+//	json.NewEncoder(os.Stdout)
+// In encoding/json - NewEncoder returns a pointer to an Encoder:
+//	func NewEncoder(w io.Writer) *Encoder
+// In encoding/json - It can therefore be used as imput for Encode:
+//	func (enc *Encoder) Encode(v any) error
+// In this function - This can be and is called with dot notation to encode the agents:
+//	json.NewEncoder(os.Stdout).Encode(agents)
+func EncodeJsonStdout(agents []agent) {
+	err := json.NewEncoder(os.Stdout).Encode(agents)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// SortNumbers sorts a slice of ints
+func SortNumbers(numbers []int) {
+	sort.Ints(numbers)
+}
+
+// SortStrings sorts a slice of strings
+func SortStrings(strings []string) {
+	sort.Strings(strings)
+}
+
+// SortPeopleAgeLast sorts people by age and then last name
+func SortPeopleAgeLast(people []person) {
+	sort.SliceStable(people, func(i, j int) bool{
+		return people[i].Age < people[j].Age
+	})
+	sort.SliceStable(people, func(i, j int) bool{
+		return people[i].Last < people[j].Last
+	})
+}
+
+// SortPeopleSayings sorts the sayings of each person in people
+func SortPeopleSayings(people []person) {
+	for _, person := range people {
+		sort.Strings(person.Sayings)
+	}
 }
--- 19/exerciseeight/exerciseeigtht_test.go
+++ 19/exerciseeight/exerciseeigtht_test.go
@@ -29,4 +29,142 @@ func ExampleUnmarshallPeople() {
 	// [{James Bond 32 [Shaken, not stirred Youth is no guarantee of innovation In his majesty's royal service]} {Miss Moneypenny 27 [James, it is soo good to see you Would you like me to take care of that for you, James? I would really prefer to be a secret agent myself.]} {M Hmmmm 54 [Oh, James. You didn't. Dear God, what has James done now? Can someone please tell me where James Bond is?]}]
 }
 
+func ExampleEncodeJsonStdout() {
+	a1 := agent{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
+
+	a2 := agent{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	a3 := agent{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+
+	agents := []agent{a1, a2, a3}
+	EncodeJsonStdout(agents)
+	// Output:
+	// [{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]
+}
+
+func ExampleSortNumbers() {
+	xi := []int{5, 8, 2, 43, 17, 987, 14, 12, 21, 1, 4, 2, 3, 93, 13}
+	SortNumbers(xi)
+	fmt.Println(xi)
+	// Output:
+	// [1 2 2 3 4 5 8 12 13 14 17 21 43 93 987]
+}
+
+func ExampleSortStrings() {
+	xs := []string{"random", "rainbow", "delights", "in", "torpedo", "summers", "under", "gallantry", "fragmented", "moons", "across", "magenta"}
+	SortStrings(xs)
+	fmt.Println(xs)
+	// Output:
+	// [across delights fragmented gallantry in magenta moons rainbow random summers torpedo under]
+}
+
+func ExampleSortPeopleAgeLast() {
+	p1 := person{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
+
+	p2 := person{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	p3 := person{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+	people := []person{p1, p2, p3}
+	SortPeopleAgeLast(people)
+	fmt.Println(people)
+	// Output:
+	// [{James Bond 32 [Shaken, not stirred Youth is no guarantee of innovation In his majesty's royal service]} {M Hmmmm 54 [Oh, James. You didn't. Dear God, what has James done now? Can someone please tell me where James Bond is?]} {Miss Moneypenny 27 [James, it is soo good to see you Would you like me to take care of that for you, James? I would really prefer to be a secret agent myself.]}]
+}
+
+func ExampleSortPeopleSayings() {
+	p3 := person{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
+
+	p4 := person{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	p5 := person{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+	people := []person{p3, p4, p5}
+	SortPeopleSayings(people)
+	fmt.Println(people)
+	// Output:
+	// [{James Bond 32 [In his majesty's royal service Shaken, not stirred Youth is no guarantee of innovation]} {Miss Moneypenny 27 [I would really prefer to be a secret agent myself. James, it is soo good to see you Would you like me to take care of that for you, James?]} {M Hmmmm 54 [Can someone please tell me where James Bond is? Dear God, what has James done now? Oh, James. You didn't.]}]
+}
+
+
+
 
