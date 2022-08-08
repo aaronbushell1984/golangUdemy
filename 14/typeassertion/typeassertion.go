af86e7acbbf6cd6eca2f32b5675fe2acd9544c93
Aaron Bushell <aaron_bushell@yahoo.com>
2022-08-08 17:39:36 +0000

    add type assertion examples

--- 14/typeassertion/typeassertion.go
+++ 14/typeassertion/typeassertion.go
@@ -1,2 +1,18 @@
 // Package typeassertion demonstrates the use of type assertions in go
 package typeassertion
+
+// IsString returns true if argument is of type string
+func IsString(s interface{}) bool {
+	s, ok := s.(string)
+	return ok
+}
+
+// IsIntOrInt64 switches on type to return true if argument is of type int or int64
+func IsIntOrInt64(n interface{}) bool {
+	switch n.(type) {
+	case int, int64:
+		return true
+	default:
+		return false
+	}	
+}
--- 14/typeassertion/typeassertion_test.go
+++ 14/typeassertion/typeassertion_test.go
@@ -1,1 +1,26 @@
 package typeassertion
+
+import "fmt"
+
+func ExampleIsString() {
+	s := "I'm a string"
+	n := 1
+	fmt.Println(IsString(s))
+	fmt.Println(IsString(n))
+	// Output:
+	// true
+	// false
+}
+
+func ExampleIsIntOrInt64() {
+	s := "I'm a string"
+	n := 1
+	var sf int64 = 64
+	fmt.Println(IsIntOrInt64(s))
+	fmt.Println(IsIntOrInt64(n))
+	fmt.Println(IsIntOrInt64(sf))
+	// Output:
+	// false
+	// true
+	// true
+}
