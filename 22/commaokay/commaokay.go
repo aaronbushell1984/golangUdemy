ae1ccf8d2d520f9e11abb460dde6bfeb22507767
Aaron Bushell <aaron_bushell@yahoo.com>
2022-08-10 21:38:26 +0000

    complete commaokay examples

--- 22/commaokay/commaokay.go
+++ 22/commaokay/commaokay.go
@@ -1,2 +1,51 @@
 // Package commaokay demonstrates the use of comma okay idiom using channels
 package commaokay
+
+// ConsumeMultipleReadOnlyChannels performs the same as that in selectchannel
+//
+// The example catches ok in the final case in select statement:
+//	case v, ok := <-q:
+//		fmt.Printf("Quitting on: %v Function ran ok?: %v", v, ok)
+// As this case is run a true is returned to ok
+func ConsumeMultipleReadOnlyChannels() (<-chan int, <-chan int, <-chan int) {
+	even := make(chan int)
+	odd := make(chan int)
+	quit := make(chan int)
+	go func(){
+		for i := 0; i < 10; i++ {
+			if i % 2 == 0 {
+				even <- i
+			}
+			if i % 2 != 0 {
+				odd <- i
+			}
+		}
+		quit <- 10
+	}()
+	return even, odd, quit
+}
+
+// ConfirmValueReadFromChannel allows consumption of 42 sent:
+//		go func() {
+//			c <- 42
+//		}()
+// ok is used to check channel receives 42 successfully 
+func ConfirmValueReadFromChannel() (int, bool) {
+	c := make(chan int)
+	go func() {
+		c <- 42
+	}()
+	v, ok := <-c
+	return v, ok
+}
+
+// ConfirmValueReadFromChannelClose replicates ConfirmValueReadFromChannel but also closes channel
+func ConfirmValueReadFromChannelClose() (int, bool) {
+	c := make(chan int)
+	go func() {
+		defer close(c)
+		c <- 42
+	}()
+	v, ok := <-c
+	return v, ok	
+}
--- 22/commaokay/commaokay_test.go
+++ 22/commaokay/commaokay_test.go
@@ -1,1 +1,36 @@
 package commaokay
+
+import "fmt"
+
+func ExampleConsumeMultipleReadOnlyChannels() {
+	e, o, q := ConsumeMultipleReadOnlyChannels()
+	for {
+		select {
+		case v := <-e:
+			fmt.Printf("Even: %v\t", v)
+		case v := <-o:
+			fmt.Printf("Odd: %v\t", v)
+		case v, ok := <-q:
+			fmt.Printf("Quitting on: %v Function ran ok?: %v", v, ok)
+			return
+		}		
+	}
+	// Output:
+	// Even: 0	Odd: 1	Even: 2	Odd: 3	Even: 4	Odd: 5	Even: 6	Odd: 7	Even: 8	Odd: 9	Quitting on: 10 Function ran ok?: true
+}
+
+func ExampleConfirmValueReadFromChannel() {
+	value, k := ConfirmValueReadFromChannel()
+	fmt.Printf("%v %v",value, k)
+	// Output:
+	// 42 true
+}
+
+func ExampleConfirmValueReadFromChannelClose() {
+	value, okay := ConfirmValueReadFromChannel()
+	fmt.Printf("%v %v", value, okay)
+	// Output:
+	// 42 true
+}
+
+
