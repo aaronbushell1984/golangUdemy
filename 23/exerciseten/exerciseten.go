c04a5f7f4773de4ec999c9918bc3b627c916775b
Aaron Bushell <aaron_bushell@yahoo.com>
2022-08-14 23:39:35 +0000

    complete exerciseten

---
+++ 23/exerciseten/exerciseten.go
@@ -0,0 +1,149 @@
+// Package exerciseten is the tenth set of exercises from the course
+package exerciseten
+
+import "fmt"
+
+// ReceiveFromChannel would block without the func literal - go routine to receive the value 42:
+//	c := make(chan int)
+//	go func() {
+//		c <- 42
+//	}()
+//	return <-c
+func ReceiveFromChannelFuncLiteral() int {
+	c := make(chan int)
+	go func() {
+		c <- 42
+	}()
+	return <-c
+}
+
+// ReceiveFromBufferedChannel would block without a buffer of exactly the right amout to receive the 42:
+//	c := make(chan int, 1)
+func ReceiveFromBufferedChannel() int {
+	c := make(chan int, 1)
+	c <- 42
+	return <-c
+}
+
+// GetValueInReceiveOnlyChannel add 42 to a channel and returns it as receive only so:
+//	<-GetValueInReceiveOnlyChannel()
+// receives the value and cannot be misused by sending information to channel
+func GetValueInReceiveOnlyChannel() <-chan int {
+	c := make(chan int)
+	go func(){
+		c <- 42
+	}()
+	return c
+}
+
+// Receive takes a receive only channel and ranges over the results to append to the resulting slice of int
+func Receive(c <-chan int) []int {
+	var numbers []int
+	for v := range c {
+		numbers = append(numbers, v)
+	}
+	return numbers
+}
+
+// Generate100 adds and returns numbers 0-99 in a receive only channel
+//
+// func literal and closing the channel on completion of loop prevents a block:
+//	go func() {
+//		for i := 0; i < 100; i++ {
+//			c <- i
+//		}
+//		close(c)
+//	}()
+func Generate100() <-chan int {
+	c := make(chan int)
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+	return c
+}
+
+// QuitWithZero returns a receive only channel with zero to be used a quit channle later
+func QuitWithZero() <-chan int {
+	q := make(chan int)
+	go func() {
+		q <- 0
+	}()
+	return q
+}
+
+// ReceiveWithSelect loops indefinitely over first provided channal until the second sends in the quit value
+//
+// All values are added to a slice of int which returns on completion using either case:
+//	var numbers []int
+//	for {
+//		select {
+//		case v := <-c:
+//			numbers = append(numbers, v)
+//		case v := <-q:
+//			numbers = append(numbers, v)
+//		return numbers
+//		}
+//	}
+func ReceiveWithSelect(c, q <-chan int) []int {
+	var numbers []int
+	for {
+		select {
+		case v := <-c:
+			numbers = append(numbers, v)
+		case v := <-q:
+			numbers = append(numbers, v)
+			return numbers
+		}
+	}
+}
+
+// GetOkFromOpenChannel shows ok is used to show a value is taken from channel successfully
+func GetOkFromOpenChannel() bool {
+	c := make(chan int)
+	go func() {
+		c <- 42
+	}()
+	_, ok := <-c
+	return ok
+}
+
+// GetOkFromClosedChannel shows that a channel which has had its value taken and is closed returns false into ok:
+//	_, ok := <-c
+//	close(c)
+//	_, ok = <-c
+func GetOkFromClosedChannel() string {
+	c := make(chan int)
+	go func() {
+		c <- 42
+	}()
+	_, ok := <-c
+	fmt.Printf("The first ok is: %v\n", ok)
+	close(c)	
+	_, ok = <-c
+	return fmt.Sprintf("The second ok is: %v", ok)
+}
+
+// FanOut10GoRoutinesCountingToTen creates and returns 10 go routines all placing 1-10 into a read only channel
+func FanOut10GoRoutinesCountingToTen() <-chan int {
+	c := make(chan int)
+	for i := 1; i < 11; i++ {
+		go func() {
+			for i := 1; i < 11; i++ {
+				c <- i
+			}
+		}() 
+	}
+	return c
+}
+
+// Fanin receives from the receive only channel 100 times and returns the values in a slice of int
+func FanIn(out <-chan int) []int {
+	var numbers []int
+	for i := 0; i < 100; i++ {
+		numbers = append(numbers, <-out)
+	}
+	return numbers
+}
---
+++ 23/exerciseten/exerciseten_test.go
@@ -0,0 +1,90 @@
+package exerciseten
+
+import (
+	"fmt"
+	"sort"
+)
+
+func ExampleReceiveFromChannelFuncLiteral() {
+	fmt.Println(ReceiveFromChannelFuncLiteral())
+	// Output:
+	// 42
+}
+
+func ExampleReceiveFromBufferedChannel() {
+	fmt.Println(ReceiveFromBufferedChannel())
+	// Output:
+	// 42
+}
+
+func ExampleGetValueInReceiveOnlyChannel() {
+	fmt.Println(<-GetValueInReceiveOnlyChannel())
+	// Output:
+	// 42
+}
+
+func ExampleReceive() {
+	c := Generate100()
+	fmt.Println(Receive(c))
+	// Output:
+	// [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99]
+}
+
+func ExampleGenerate100() {
+	numbers := Receive(Generate100())
+	fmt.Println(numbers)
+	// Output:
+	// [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99]
+}
+
+func ExampleReceiveWithSelect() {
+	q := QuitWithZero()
+	c := Generate100()
+	numbers := ReceiveWithSelect(c, q)
+	fmt.Println(numbers[len(numbers)-1])
+	// Output:
+	// 0
+}
+
+func ExampleQuitWithZero() {
+	fmt.Println(<-QuitWithZero())
+	// Output:
+	// 0
+}
+
+func ExampleGetOkFromOpenChannel() {
+	fmt.Println(GetOkFromOpenChannel())
+	// Output:
+	// true
+}
+
+func ExampleGetOkFromClosedChannel() {
+	fmt.Println(GetOkFromClosedChannel())
+	// Output:
+	// The first ok is: true
+	// The second ok is: false
+}
+
+func ExampleFanOut10GoRoutinesCountingToTen() {
+	fmt.Println(<-FanOut10GoRoutinesCountingToTen())
+	// Output:
+	// 1
+}
+
+func ExampleFanIn() {
+	out := FanOut10GoRoutinesCountingToTen()
+	numbers := FanIn(out)
+	sort.Ints(numbers)
+	fmt.Println(numbers)
+	// Output:
+	// [1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 3 3 3 4 4 4 4 4 4 4 4 4 4 5 5 5 5 5 5 5 5 5 5 6 6 6 6 6 6 6 6 6 6 7 7 7 7 7 7 7 7 7 7 8 8 8 8 8 8 8 8 8 8 9 9 9 9 9 9 9 9 9 9 10 10 10 10 10 10 10 10 10 10]
+}
+
+
+
+
+
+
+
+
+
