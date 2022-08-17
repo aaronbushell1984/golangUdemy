8b7290476b9bf37019510027d5f6945b435c4fa5
Aaron Bushell <aaron_bushell@yahoo.com>
2022-08-17 00:00:28 +0000

    start exerciseeleven

--- 25/exerciseeleven/exerciseeleven.go
+++ 25/exerciseeleven/exerciseeleven.go
@@ -1,2 +1,46 @@
 // Package exerciseeleven is the eleventh set of exercises from the course
 package exerciseeleven
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First	string
+	Last	string
+	Sayings	[]string
+}
+
+type errCustom struct {
+	prompt 	string
+	err		error
+}
+
+// MarshalPerson takes a value of type person struct and encodes to a string
+//
+// It demonstrates handling the potential error:
+//	if err != nil {
+//		return fmt.Sprintf("There was an error: %v", err)
+//	}
+func MarshalPerson(p person) (string, error) {
+	bs, err := json.Marshal(p)
+	if err != nil {
+		return "Failed", fmt.Errorf("there was an error: %v", err)
+	}
+	return string(bs), err
+}
+
+// JsonPerson marshals a person to json and demonstrates using Errof from fmt:
+//	return "Failed", fmt.Errorf("there was an error: %v", err)
+func JsonPerson(p person) (string, error) {
+	bs, err := MarshalPerson(p)
+	if err != nil {
+		return "Failed", fmt.Errorf("there was an error: %v", err)
+	}
+	return string(bs), err
+}
+
+func (e errCustom) GetEven(numbers []int) ([]int, error) {
+	Error()
+}
--- 25/exerciseeleven/exerciseeleven_test.go
+++ 25/exerciseeleven/exerciseeleven_test.go
@@ -1,1 +1,35 @@
 package exerciseeleven
+
+import "fmt"
+
+func ExampleMarshalPerson() {
+	jb := person{
+		First:		"James",
+		Last:		"Bond",
+		Sayings:	[]string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+	fmt.Println(MarshalPerson(jb))
+	// Output:
+	// {"First":"James","Last":"Bond","Sayings":["Shaken, not stirred","Any last wishes?","Never say never"]} <nil>
+}
+
+func ExampleJsonPerson() {
+	jb := person{
+		First:		"James",
+		Last:		"Bond",
+		Sayings:	[]string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+	fmt.Println(JsonPerson(jb))
+	// Output:
+	// {"First":"James","Last":"Bond","Sayings":["Shaken, not stirred","Any last wishes?","Never say never"]} <nil>
+}
+
+func ExampleGetEven() {
+	numbers := []int{1, 2, 3, 4}
+	evens, err := GetEven(numbers)
+	fmt.Println(evens)
+	// Output:
+	// [2, 4]
+}
+
+
