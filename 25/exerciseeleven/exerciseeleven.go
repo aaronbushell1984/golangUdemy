4c6651a5f40dc01b84968abfe47ef0fd2dce20ad
Aaron Bushell <aaron_bushell@yahoo.com>
2022-08-17 22:06:30 +0000

    complete exerciseeleven

--- 25/exerciseeleven/exerciseeleven.go
+++ 25/exerciseeleven/exerciseeleven.go
@@ -3,7 +3,9 @@ package exerciseeleven
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"math"
 )
 
 type person struct {
@@ -12,8 +14,13 @@ type person struct {
 	Sayings	[]string
 }
 
-type errCustom struct {
+type ErrCustom struct {
 	prompt 	string
+}
+
+type SqrtError struct {
+	pack	string
+	meth	string
 	err		error
 }
 
@@ -41,6 +48,55 @@ func JsonPerson(p person) (string, error) {
 	return string(bs), err
 }
 
-func (e errCustom) GetEven(numbers []int) ([]int, error) {
-	Error()
+// ErrCustom_Error attaches to ErrCustom to a the package and the custom prompt
+//
+// This method allows a call of the method on any error type
+func (e ErrCustom) Error() string {
+	return fmt.Sprintf("exerciseeleven error: %v", e.prompt)
+}
+
+// GetEven returns even numbers and allows a custom error ie:
+//	ceGe := ErrCustom{
+//		prompt: "geteven error",
+//	}
+// to be passed in as type error (polymorphism):
+//	evens, err := GetEven(numbers, ceGe)
+// This is due to the errCustom struct having method signature:
+//	Error() string
+// which satisfies the standard library error interface and therefore implicitly implements the interface
+func GetEven(numbers []int, e error) ([]int, string) {
+	var evens []int
+	for _, v := range numbers {
+		if v % 2 == 0 {
+			evens = append(evens, v)
+		}
+		if v == 0 {
+			return evens, e.Error()
+		}
+	}
+	return evens, ""
+}
+
+// Assertion demonstrates that a type may be asserted from error to ErrCustom using:
+//	e.(ErrCustom)
+// This allows access to a field on the ErrCustom struct (prompt) via:
+//	e.(ErrCustom).prompt
+// Direct access of prompt from the error value would not work:
+//	e.prompt	
+func Assertion(e error) string {
+	return e.(ErrCustom).prompt
+}
+
+// SqrtError_Error returns a string with package, method and error attached to any struct value of type sqrtError
+func (se SqrtError) Error() string {
+	return fmt.Sprintf("-package-%v -method-%v -error-%v", se.pack, se.meth, se.err)
+}
+
+// Sqrt returns the square root unless the number is less than or equal to zero where a sqrtError is returned
+func Sqrt(n float64) (float64, error) {
+	if n <= 0 {
+		e := errors.New("cannot supply a number less than or equal to zero")
+		return 0, SqrtError{pack: "exerciseeleven", meth: "sqrt", err: e,}
+	}
+	return math.Sqrt(n), nil
 }
--- 25/exerciseeleven/exerciseeleven_test.go
+++ 25/exerciseeleven/exerciseeleven_test.go
@@ -24,12 +24,56 @@ func ExampleJsonPerson() {
 	// {"First":"James","Last":"Bond","Sayings":["Shaken, not stirred","Any last wishes?","Never say never"]} <nil>
 }
 
-func ExampleGetEven() {
-	numbers := []int{1, 2, 3, 4}
-	evens, err := GetEven(numbers)
-	fmt.Println(evens)
+func ExampleErrCustom_Error() {
+	ce := ErrCustom{
+		prompt: "test custom error",
+	}
+	fmt.Println(ErrCustom.Error(ce))
 	// Output:
-	// [2, 4]
+	// exerciseeleven error: test custom error
 }
 
+func ExampleGetEven() {
+	numbers := []int{1, 2, 3, 4}
+	badNumbers := []int{0, 0, 0, 0}
+	ceGe := ErrCustom{
+		prompt: "geteven error",
+	}
+	badEvens, zeroErr := GetEven(badNumbers, ceGe)
+	fmt.Println(badEvens, zeroErr)
+	evens, err := GetEven(numbers, ceGe)
+	fmt.Println(evens, err)
+	// Output:
+	// [0] exerciseeleven error: geteven error
+	// [2 4]
+}
 
+func ExampleAssertion() {
+	ceAe := ErrCustom{
+		prompt: "asserted error",
+	}
+	fmt.Println(Assertion(ceAe))
+	// Output:
+	// asserted error
+}
+
+func ExampleSqrtError_Error() {
+	se := SqrtError{
+		pack: "exerciseelven_test",
+		meth: "Example_SqrtError_Error",
+		err: nil,
+	}
+	fmt.Println(SqrtError.Error(se))
+	// Output:
+	// -package-exerciseelven_test -method-Example_SqrtError_Error -error-<nil>
+}
+
+func ExampleSqrt() {
+	v1, err1 := Sqrt(16)
+	v2, err2 := Sqrt(-10.23)
+	fmt.Println(v1, err1)
+	fmt.Println(v2, err2)
+	// Output:
+	// 4 <nil>
+	// 0 -package-exerciseeleven -method-sqrt -error-cannot supply a number less than or equal to zero
+}
