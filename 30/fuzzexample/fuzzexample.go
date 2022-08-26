abda8eb9a2fe882ce03bfbb5dde3d6f836931428
Aaron <aaron_bushell@yahoo.com>
2022-08-26 22:27:59 +0100

    complete fuzzing example

--- 30/fuzzexample/fuzzexample.go
+++ 30/fuzzexample/fuzzexample.go
@@ -1,10 +1,66 @@
 // Package fuzzexample demonstrates use of a fuzz test in go
+//
+// Fuzzing the square root function reveals issues of imaginary numbers handled with:
+//
+//	if number < 0 {
+//		number := math.Abs(number)
+//		imaginary := math.Sqrt(number)
+//		return err, imaginary, "i"
+//	}
+//
+// Precision is revealed in fuzzing and handled by restricting SquareRoot:
+//
+//	if number > 999999999 {
+//		return fmt.Errorf("number above maximum allowed"), 0, ""
+//	}
+//
+// This error is skipped in fuzzing using:
+//
+//	if err != nil {
+//		return
+//	}
+//
+// The output is truncated for reasonable display and a higher precision used to ensure always accurate:
+//
+//	before := fmt.Sprintf("%.2f", math.Round(n*1000000)/1000000)
+//	after := fmt.Sprintf("%.2f", math.Round(reSquared*1000000)/1000000)
 package fuzzexample
 
-func Add(numbers ...int) int {
-	var total int
-	for _, v := range numbers {
-		total += v
+import (
+	"fmt"
+	"math"
+)
+
+// SquareRoot uses:
+//
+//	math.Sqrt(number)
+//
+// to provide a square root
+//
+//	"number above maximum allowed"
+//
+// is returned as an error if a number over 999999999 is provided
+//
+//	"i"
+//
+// is returned as a string to signify imaginary number if provided number is negative
+func SquareRoot(number float64) (error, float64, string) {
+	var err error
+	if number > 999999999 {
+		return fmt.Errorf("number above maximum allowed"), 0, ""
 	}
-	return total
+	if number < 0 {
+		number := math.Abs(number)
+		imaginary := math.Sqrt(number)
+		return err, imaginary, "i"
+	}
+	return err, math.Sqrt(number), ""
+}
+
+// Square squares a number. If the number is imaginary then the result is negative
+func Square(number float64, imaginary string) float64 {
+	if imaginary == "i" {
+		return -(number * number)
+	}
+	return number * number
 }
--- 30/fuzzexample/fuzzexample_test.go
+++ 30/fuzzexample/fuzzexample_test.go
@@ -2,20 +2,48 @@ package fuzzexample
 
 import (
 	"fmt"
+	"math"
 	"testing"
 )
 
-func ExampleAdd() {
-	fmt.Print(Add(1, 2, 3))
+func ExampleSquareRoot() {
+	_, root, _ := SquareRoot(16)
+	fmt.Println(root)
+	_, imaginaryBase, i := SquareRoot(-16)
+	fmt.Println(imaginaryBase, i)
 	// Output:
-	// 6
+	// 4
+	// 4 i
 }
 
-func FuzzAdd(f *testing.F) {
-	f.Add(1)
-	f.Fuzz(func(t *testing.T, ns int) {
-		out := Add(ns)
-		if
-		t.Errorf("%q", out)
+func ExampleSquare() {
+	fmt.Println(Square(4, ""))
+	fmt.Println(Square(4, "i"))
+	// Output:
+	// 16
+	// -16
+}
+
+func FuzzSquareRoot(f *testing.F) {
+	testcases := []float64{16, 4, 32}
+	for _, v := range testcases {
+		f.Add(v)
+	}
+	f.Fuzz(func(t *testing.T, n float64) {
+		var reSquared float64
+		err, squareRoot, i := SquareRoot(n)
+		if err != nil {
+			return
+		}
+		if i == "i" {
+			reSquared = Square(squareRoot, "i")
+		} else {
+			reSquared = Square(squareRoot, "")
+		}
+		before := fmt.Sprintf("%.2f", math.Round(n*1000000)/1000000)
+		after := fmt.Sprintf("%.2f", math.Round(reSquared*1000000)/1000000)
+		if before != after {
+			t.Errorf("Before: %v After: %v, Error: %v", before, after, err)
+		}
 	})
 }
