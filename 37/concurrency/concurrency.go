1f29dada0ecf1736ac82e8594f1ab9b12c80c79a
Aaron <aaron_bushell@yahoo.com>
2022-08-28 16:52:50 +0100

    add documentation to concurrency.go

--- 37/concurrency/concurrency.go
+++ 37/concurrency/concurrency.go
@@ -1,16 +1,21 @@
+// Package concurrency demonstrates/simulates potentially improving speed of multiple sequential web calls
+//
+// Adding a concurrency pattern using goroutines and channels allows the program to run in parallel if hardware and conditions allow
 package concurrency
 
+// WebsiteChecker simulates (in this example a function which would check the status of a website
 type WebsiteChecker func(string) bool
 
+// result struct defines the type to hold website url string and if the site was accessible when received from a channel
 type result struct {
 	website      string
 	isAccessible bool
 }
 
+// CheckWebsites was refactored during this exercise to create a goroutine for each WebsiteChecker call on url
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 	resultsChannel := make(chan result)
-
 	for _, url := range urls {
 		go func(u string) {
 			resultsChannel <- result{u, wc(u)}
