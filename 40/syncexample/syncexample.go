14f6015287b8cb642753912fe6c2124af6e019f4
Aaron <aaron_bushell@yahoo.com>
2022-08-29 12:44:45 +0100

    complete syncexample

--- 40/syncexample/syncexample.go
+++ 40/syncexample/syncexample.go
@@ -1,1 +1,46 @@
 package syncexample
+
+import "sync"
+
+// Counter contains a state with value:
+//
+//	value int
+//
+// Channels are best used for passing data ownership, mutex is a great way of managing state:
+//
+//	mu    sync.Mutex
+//
+// NB. It is not advisable to embed a mutex as it allows public access to lock and unlock:
+//
+//	sync.Mutex
+//
+// NB. Counter contains a mutex so is best used with pointers to avoid copying. A constructor has therefore been included to encourage its use.
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+// Inc locks the value, defers unlocking it and increments the value by one
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+// Value returns the current value/count
+//
+//	return c.value
+func (c *Counter) Value() int {
+	return c.value
+}
+
+// NewCounter returns the address of a newly created Counter
+//
+//	return &Counter{}
+//
+// NB. construction with declaration and assignment is not encouraged as a non pointer value may lead to passing a lock by value:
+//
+//	Please Avoid: counter := Counter{}
+func NewCounter() *Counter {
+	return &Counter{}
+}
--- 40/syncexample/syncexample_test.go
+++ 40/syncexample/syncexample_test.go
@@ -1,1 +1,36 @@
 package syncexample
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing 3 times leaves at 3", func(t *testing.T) {
+		counter := NewCounter()
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+		asserCounter(t, counter, 3)
+	})
+	t.Run("runs safe concurrently", func(t *testing.T) {
+		want := 1000
+		counter := NewCounter()
+		var wg sync.WaitGroup
+		wg.Add(want)
+		for i := 0; i < want; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		asserCounter(t, counter, want)
+	})
+}
+
+func asserCounter(t *testing.T, counter *Counter, want int) {
+	if counter.Value() != want {
+		t.Errorf("got: %d want: %d", counter.Value(), want)
+	}
+}
