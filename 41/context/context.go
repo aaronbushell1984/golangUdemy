8ff3ebebbd4616207af3a630802393b2120127b8
Aaron <aaron_bushell@yahoo.com>
2022-08-29 23:55:03 +0100

    complete context

---
+++ 41/context/context.go
@@ -0,0 +1,30 @@
+package context
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Store defines behaviour to fetch and cancel
+//
+// Context is passed as an argument which is idiomatic and mandated by google for all its programs
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+	Cancel()
+}
+
+// Server accepts a store to pass to and return a function which uses a writer to print a requests' response
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+		if err != nil {
+			log.Println("error fetching from store")
+		}
+		_, err = fmt.Fprint(w, data)
+		if err != nil {
+			log.Println("error writing data")
+		}
+	}
+}
